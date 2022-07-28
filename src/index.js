ed2ae4fa0c472c3ff677bd490a98bfefff891e08
kpavann <kulkarnipavan6115@gmail.com>
2022-07-29 01:41:36 +0530

    Inital Commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { makeServer } from "./server";
+import { BrowserRouter as Router } from "react-router-dom";
+import { DataProvider } from "./context/data/data-context";
+
+
+// Call make Server
+makeServer();
+
+ReactDOM.render(
+  <React.StrictMode>
+      <DataProvider>
+      <Router>
+          <App />
+      </Router>
+      </DataProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
