98088ad545a461024f20144ab2a1b98a11a94010
kpavann <kulkarnipavan6115@gmail.com>
2022-09-21 01:35:47 +0530

    refactor: added 404page

---
+++ src/context/index.js
@@ -0,0 +1,4 @@
+export { useAuth, AuthProvider } from './auth/auth-context'
+export {useCart, CartProvider} from './cart/cart-context' 
+export {useData, DataProvider} from './data/data-context'
+export { useWishlist, WishListProvider} from './wishlist/wishlist-context'
