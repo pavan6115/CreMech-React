c66a392905773c323a612b8e814697cebfb0c9cd
kpavann <kulkarnipavan6115@gmail.com>
2022-09-21 01:43:56 +0530

    refactor: barrel exports

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
-import { AuthProvider } from './context/auth/auth-context'
-import { CartProvider } from './context/cart/cart-context'
-import { DataProvider } from './context/data/data-context'
-import { WishListProvider } from './context/wishlist/wishlist-context'
+import {
+  AuthProvider,
+  CartProvider,
+  DataProvider,
+  WishListProvider,
+} from './context/index'
 import './index.css'
 import { makeServer } from './server'
 
